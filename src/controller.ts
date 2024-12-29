f95b106ed4dd226c3cd334ed305ae556979010a0
Boris Saranin <saraninb@mail.ru>
2024-12-29 21:19:42 +0500

    Abstract controller class

--- src/controller.ts
+++ src/controller.ts
@@ -1,3 +1,19 @@
-export abstract class Controller {
+import type Hero from "@ulixee/hero";
+import type {Core} from "./core";
+import type {ILogger} from "./logger";
 
+export type Controller = new () => AbstractController;
+export abstract class AbstractController {
+    protected browser: Hero;
+    protected core: Core;
+    protected logger: ILogger;
+    abstract readonly name: string;
+
+    protected constructor(browser: Hero, core: Core) {
+        this.browser = browser;
+        this.core = core;
+        this.logger = core.logger;
+    }
+
+    abstract async handle(input: { [p: string]: any }): Promise<void>
 }
